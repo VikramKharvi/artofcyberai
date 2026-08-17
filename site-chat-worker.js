39248cb47b82a0fe37906ac5a8d4c31b8b0703d1
VikramKharvi <vikram984511@gmail.com>
2026-08-16 17:52:02 -0700

    Add field-notes assistant to homepage

---
+++ site-chat-worker.js
@@ -0,0 +1,5 @@
+import { WebWorkerMLCEngineHandler } from "https://esm.run/@mlc-ai/web-llm";
+
+const handler = new WebWorkerMLCEngineHandler();
+self.onmessage = (message) => handler.onmessage(message);
+
